ea51410afcd28a55c81146fa72a59b17bd6ceaec
Oliver Staton <oliver.william.staton@gmail.com>
2019-07-20 15:56:09 -0700

    https://www.youtube.com/watch?v=JcI5Vnw0b2c&list=PLQVvvaa0QuDfKTOs3Keq_kaG2P55YRn5v

---
+++ example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import pandas as pandas
+import quandl
+
+df = quandl.get("WIKI/TSLA", authtoken="nJYkQ_X6C1RRzvcttWUy")
+
+df = df[['Adj. Open','Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+
+print(df.head())
